e4c7c56bf6b524d5ad46b21cedf07aa9bac94e20
takeYY <t0k6yist@gmail.com>
2024-08-11 12:12:53 +0900

    tests: add health check test

---
+++ tests/integration/health/health_handler_test.go
@@ -0,0 +1,42 @@
+package health
+
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+
+	"filmogophery/internal/config"
+	"filmogophery/internal/health"
+)
+
+func TestHealth(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/health", nil)
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+
+	c := e.NewContext(req, rec)
+	c.SetPath("/health")
+
+	// 設定ファイルの読み込み
+	conf, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
+	}
+	handler := health.NewHandler(conf)
+	er := handler.ReaderHandler.Health(c)
+	if er != nil {
+		t.Fatal(er)
+	}
+
+	if assert.NoError(t, er) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+
+		expected := `{"message":"system all green"}`
+		assert.JSONEq(t, expected, rec.Body.String())
+	}
+}
