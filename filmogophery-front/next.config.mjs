2d601cd5ec80c02cfd30abeeb6cc3edfbd0b97f3
takeYY <t0k6yist@gmail.com>
2024-08-14 18:19:30 +0900

    init: NextJS

---
+++ filmogophery-front/next.config.mjs
@@ -0,0 +1,14 @@
+const nextConfig = {
+  images: {
+    remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'image.tmdb.org',
+          port: '',
+          pathname: '/t/p/**',
+        },
+      ],
+  }
+}
+
+export default nextConfig;
