809444b783cebcff76826bb21be728e8c89b1f61
takeYY <t0k6yist@gmail.com>
2024-08-15 02:13:04 +0900

    add: CORS settings

--- filmogophery-front/next.config.mjs
+++ filmogophery-front/next.config.mjs
@@ -1,3 +1,4 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -8,7 +9,21 @@ const nextConfig = {
           pathname: '/t/p/**',
         },
       ],
-  }
+  },
+  // 全ての API routes にマッチ
+  async headers() {
+    return [
+      {
+        source: '/media',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig;
