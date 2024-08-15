9dd3655868e16349ecc553c79e2707bd71f774b9
takeYY <t0k6yist@gmail.com>
2024-08-15 18:40:11 +0900

    change: from inmemory to calling api

--- filmogophery-front/src/app/page.tsx
+++ filmogophery-front/src/app/page.tsx
@@ -1,12 +1,34 @@
 "use client";
 
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { Movie } from "@/interface/movie";
+import { Movie, Genre } from "@/interface/movie";
 
 export default function Home() {
   const router = useRouter();
-  const movies = fetchMovies();
+  const [movies, setMovies] = useState<Movie[]>();
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(`/api/movies`, { method: "GET" });
+        const movies: Movie[] = await response.json();
+        console.log("moviesのデータ取得: 完了");
+
+        return setMovies(movies);
+      } catch {
+        console.log("moviesのデータ取得: エラー。空配列で定義します");
+        return setMovies([]);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  if (!movies) {
+    return <div></div>;
+  }
   return (
     <main>
       <div className="container pb-4">
@@ -15,18 +37,23 @@ export default function Home() {
         {/* TODO: レイアウトが崩れているので、直すこと!! */}
         <div className="card-columns">
           {movies &&
-            movies.map((obj: Movie, index: number) => {
+            movies.map((movie: Movie, index: number) => {
               return (
                 <button
                   key={index}
                   className="card bg-dark"
-                  onClick={() => router.push(`/movie/${obj.id}`)}
+                  onClick={() => router.push(`/movie/${movie.id}`)}
                 >
                   <div className="row no-gutters">
                     {/* ポスター */}
                     <div className="col-md-4">
                       <Image
-                        src={obj.posterURL}
+                        src={
+                          `https://image.tmdb.org/t/p/w600_and_h900_bestv2` +
+                          (movie.poster
+                            ? movie.poster.url
+                            : "/Agz71U0wcesx87micVn731Z1vPu.jpg")
+                        }
                         alt="ポスター画像"
                         className="img-fluid"
                         width={75}
@@ -36,15 +63,15 @@ export default function Home() {
                     <div className="col-md-8">
                       <div className="card-body">
                         {/* タイトル */}
-                        <h5 className="card-title text-light">{obj.title}</h5>
+                        <h5 className="card-title text-light">{movie.title}</h5>
                         {/* 概要 */}
-                        <p className="card-text text-light">{obj.overview}</p>
+                        <p className="card-text text-light">{movie.overview}</p>
                         {/* ジャンル */}
-                        {obj.genres &&
-                          obj.genres.map((g: string, i: number) => {
+                        {movie.genres &&
+                          movie.genres.map((genre: Genre, i: number) => {
                             return (
                               <p key={i} className="badge text-bg-secondary">
-                                {g}
+                                {genre.name}
                               </p>
                             );
                           })}
@@ -58,62 +85,4 @@ export default function Home() {
       </div>
     </main>
   );
-}
-
-function fetchMovies(): Movie[] | undefined {
-  try {
-    console.log("データ取得中...");
-    const movies: Movie[] = [
-      {
-        id: 1,
-        title: "ターミネーター",
-        overview:
-          "アメリカのとある街、深夜突如奇怪な放電と共に屈強な肉体をもった男が現れる。同じく...",
-        release_date: "1985-05-04",
-        run_time: 108,
-        genres: ["アクション", "SF"],
-        posterURL:
-          "https://image.tmdb.org/t/p/w600_and_h900_bestv2/iK2YBfD7DdaNXZALQhhT9uTN9Rc.jpg",
-      },
-      {
-        id: 2,
-        title: "ターミネーター2",
-        overview:
-          "未来からの抹殺兵器ターミネーターを破壊し、近未来で恐ろしい戦争が起こる事を知って...",
-        release_date: "1991-08-24",
-        run_time: 137,
-        genres: [],
-        posterURL:
-          "https://image.tmdb.org/t/p/w600_and_h900_bestv2/oCwo7ALD3LftLqy0Oj6U669u4fU.jpg",
-      },
-      {
-        id: 3,
-        title: "ターミネーター3",
-        overview:
-          "スカイネットを破壊、予見されていた最終戦争の日も過ぎたが、ジョンの心には母親から...",
-        release_date: "2003-07-12",
-        run_time: 110,
-        genres: [],
-        posterURL:
-          "https://image.tmdb.org/t/p/w600_and_h900_bestv2/oCwo7ALD3LftLqy0Oj6U669u4fU.jpg",
-      },
-      {
-        id: 4,
-        title: "ターミネーター4",
-        overview:
-          "“審判の日”から10年後の2018年。人類軍の指導者となり、機械軍と戦うことを幼...",
-        release_date: "2009-06-05",
-        run_time: 114,
-        genres: ["アクション", "SF", "戦争"],
-        posterURL:
-          "https://image.tmdb.org/t/p/w600_and_h900_bestv2/7fmBDyHsLTyhfRSZk19wrY23zDg.jpg",
-      },
-    ];
-    // const response = await fetch(`http://localhost:8000/movies`);
-    // const data = await response.json();
-    console.log("データ取得: 完了!!!");
-    return movies;
-  } catch (error) {
-    console.log("データ取得エラー");
-  }
 }
