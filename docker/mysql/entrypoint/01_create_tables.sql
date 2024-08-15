8aad19869bd5b4fe582b9a3d52f4bc47c23654ae
takeYY <t0k6yist@gmail.com>
2024-08-15 18:32:41 +0900

    chore: change db definition

--- docker/mysql/entrypoint/01_create_tables.sql
+++ docker/mysql/entrypoint/01_create_tables.sql
@@ -25,10 +25,10 @@ CREATE TABLE
 CREATE TABLE
     `movie` (
         `id` int AUTO_INCREMENT,
-        `title` varchar(255) not null,
+        `title` varchar(255) NOT NULL,
         `overview` text,
-        `release_date` date not null,
-        `run_time` int not null,
+        `release_date` date NOT NULL,
+        `run_time` int NOT NULL,
         `poster_id` int,
         `series_id` int,
         `tmdb_id` int,
@@ -40,8 +40,8 @@ CREATE TABLE
 CREATE TABLE
     `movie_genres` (
         `id` int AUTO_INCREMENT,
-        `movie_id` int not null,
-        `genre_id` int not null,
+        `movie_id` int NOT NULL,
+        `genre_id` int NOT NULL,
         primary key (`id`),
         FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`) on delete cascade,
         FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`) on delete cascade
@@ -58,9 +58,9 @@ CREATE TABLE
 CREATE TABLE
     `movie_impression` (
         `id` int AUTO_INCREMENT,
-        `movie_id` int,
+        `movie_id` int NOT NULL,
         `status` tinyint (1) NOT NULL DEFAULT 0,
-        `rating` tinyint (1),
+        `rating` float (2, 1),
         `note` TEXT,
         PRIMARY KEY (`id`),
         FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`) ON DELETE CASCADE
@@ -69,8 +69,10 @@ CREATE TABLE
 CREATE TABLE
     `movie_watch_record` (
         `id` int AUTO_INCREMENT,
-        `watch_media_id` int,
-        `watch_date` date,
+        `movie_impression_id` int NOT NULL,
+        `watch_media_id` int NOT NULL,
+        `watch_date` date NOT NULL,
         PRIMARY KEY (`id`),
+        FOREIGN KEY (`movie_impression_id`) REFERENCES `movie_impression` (`id`) ON DELETE CASCADE,
         FOREIGN KEY (`watch_media_id`) REFERENCES `watch_media` (`id`) ON DELETE CASCADE
     );
