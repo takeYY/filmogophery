c6f90517006df2a9e02b503b19ac73808d344849
takeYY <t0k6yist@gmail.com>
2024-08-15 18:39:17 +0900

    chore: change config about CORS

--- filmogophery-front/next.config.mjs
+++ filmogophery-front/next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/media',
+        source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
