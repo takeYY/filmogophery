14ec7f37d359d8201a7be078fc6b4b572a5c4b50
takeYY <t0k6yist@gmail.com>
2024-08-17 19:28:05 +0900

    chore: change import from

--- filmogophery-front/src/app/layout.tsx
+++ filmogophery-front/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { NavLinks } from "@/app/ui/nav-links";
+import { NavLinks } from "@/app/components/nav-links";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- filmogophery-front/src/app/movie/[id]/page.tsx
+++ filmogophery-front/src/app/movie/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import StarRating from "@/app/ui/Rating";
+import StarRating from "@/app/components/Rating";
 import { MovieDetail, WatchRecord } from "@/interface/movie";
 import Image from "next/image";
 import Link from "next/link";
