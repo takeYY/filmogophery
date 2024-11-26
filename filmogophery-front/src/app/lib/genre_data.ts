07c75b269497dd3e1a8adcd2e117889df6473f11
takeYY <t0k6yist@gmail.com>
2024-11-26 22:53:30 +0900

    add: mock data

---
+++ filmogophery-front/src/app/lib/genre_data.ts
@@ -0,0 +1,41 @@
+import { Genre } from "@/interface/movie";
+
+export const action: Genre = {
+  code: "action",
+  name: "アクション",
+};
+
+export const adventure: Genre = {
+  code: "adventure",
+  name: "アドベンチャー",
+};
+
+export const comedy: Genre = {
+  code: "comedy",
+  name: "コメディ",
+};
+
+export const crime: Genre = {
+  code: "crime",
+  name: "クライム",
+};
+
+export const horror: Genre = {
+  code: "horror",
+  name: "ホラー",
+};
+
+export const romance: Genre = {
+  code: "romance",
+  name: "ロマンス",
+};
+
+export const sf: Genre = {
+  code: "sf",
+  name: "SF",
+};
+
+export const war: Genre = {
+  code: "war",
+  name: "戦争",
+};
---
+++ filmogophery-front/src/app/lib/watch_media.ts
@@ -0,0 +1,15 @@
+import { WatchMedia } from "@/interface/movie";
+
+export const watchMediaData: WatchMedia[] = [
+  { id: 1, code: "prime_video", name: "Prime Video" },
+  { id: 2, code: "netflix", name: "Netflix" },
+  { id: 3, code: "u_next", name: "U-NEXT" },
+  { id: 4, code: "disney_plus", name: "Disney+" },
+  { id: 5, code: "youtube", name: "YouTube" },
+  { id: 6, code: "apple_tv", name: "Apple TV+" },
+  { id: 7, code: "hulu", name: "Hulu" },
+  { id: 8, code: "d_anime", name: "dアニメ" },
+  { id: 9, code: "telasa", name: "TELASA" },
+  { id: 10, code: "cinema", name: "映画館" },
+  { id: 99, code: "unknown", name: "不明" },
+];
