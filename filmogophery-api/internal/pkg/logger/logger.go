328d84415aefe5e228125149d41ad825ee96283f
takeYY <t0k6yist@gmail.com>
2025-01-06 21:00:44 +0900

    change: logger

--- filmogophery-api/internal/pkg/logger/logger.go
+++ filmogophery-api/internal/pkg/logger/logger.go
@@ -2,20 +2,31 @@ package logger
 
 import (
 	"os"
+	"time"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+
+	"filmogophery/internal/config"
 )
 
 // zerolog のロガーを初期化
-func InitializeLogger(logLevel string) {
-	// 標準出力に出力
-	log.Logger = zerolog.New(os.Stdout).With().Timestamp().Logger()
+func InitializeLogger(conf *config.Logger) {
+	// 標準出力
+	if conf.Format == "json" {
+		log.Logger = zerolog.New(os.Stdout).With().Timestamp().Logger()
+	} else {
+		log.Logger = zerolog.New(zerolog.ConsoleWriter{
+			Out:        os.Stderr,
+			TimeFormat: "2006-01-02 15:04:05.000000",
+		}).With().Timestamp().Logger()
+	}
+	zerolog.TimeFieldFormat = time.RFC3339Nano
 
 	// ログレベルの設定
-	level, err := zerolog.ParseLevel(logLevel)
+	level, err := zerolog.ParseLevel(conf.Level)
 	if err != nil {
-		log.Logger.Error().Msgf("Invalid log level %s, defaulting to info", logLevel)
+		log.Logger.Error().Msgf("Invalid log level %s, defaulting to info", conf.Level)
 		level = zerolog.InfoLevel
 	}
 	log.Logger = log.Logger.Level(level)
