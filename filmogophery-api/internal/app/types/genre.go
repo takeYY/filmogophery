d51c21dde97623e75cc72f95567153e0198ebffa
takeYY <t0k6yist@gmail.com>
2025-01-06 20:58:09 +0900

    add: a factory of Genre

--- filmogophery-api/internal/app/types/genre.go
+++ filmogophery-api/internal/app/types/genre.go
@@ -1,8 +1,22 @@
 package types
 
+import "filmogophery/internal/pkg/gen/model"
+
 type (
 	Genre struct {
 		Code string `json:"code"`
 		Name string `json:"name"`
 	}
 )
+
+func NewGenresByModel(genres []*model.Genre) []Genre {
+	result := make([]Genre, 0, len(genres))
+	for _, g := range genres {
+		result = append(result, Genre{
+			Code: g.Code,
+			Name: *g.Name,
+		})
+	}
+
+	return result
+}
