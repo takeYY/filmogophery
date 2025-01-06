d42efeeabd776b79d4ad1e461d3579fd751cf6d0
takeYY <t0k6yist@gmail.com>
2025-01-06 20:55:50 +0900

    add: movie service

---
+++ filmogophery-api/internal/app/services/movie_service.go
@@ -0,0 +1,42 @@
+package services
+
+import (
+	"context"
+
+	"filmogophery/internal/app/repositories"
+	"filmogophery/internal/pkg/gen/model"
+	"filmogophery/internal/pkg/logger"
+)
+
+type (
+	IMovieService interface {
+		// --- Read --- //
+
+		// 映画を全て取得
+		GetMovies(ctx context.Context) ([]*model.Movie, error)
+	}
+
+	movieService struct {
+		movieRepo repositories.IMovieRepository
+	}
+)
+
+func NewMovieService(
+	movieRepo repositories.IMovieRepository,
+) IMovieService {
+	return &movieService{
+		movieRepo,
+	}
+}
+
+func (s *movieService) GetMovies(ctx context.Context) ([]*model.Movie, error) {
+	logger := logger.GetLogger()
+
+	movies, err := s.movieRepo.FindAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+	logger.Debug().Msg("successfully fetch movies")
+
+	return movies, err
+}
