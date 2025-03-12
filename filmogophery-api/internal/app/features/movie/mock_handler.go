69b3bf1fa1b851bb0e7172f34226cddf238e564f
takeYY <t0k6yist@gmail.com>
2025-03-12 22:03:15 +0900

    chore: add mock handler

--- filmogophery-api/internal/app/features/movie/mock_handler.go
+++ filmogophery-api/internal/app/features/movie/mock_handler.go
@@ -17,3 +17,20 @@ func BuildMockedGetMoviesHandler() func(c echo.Context) error {
 		return c.JSON(http.StatusOK, mock.MockedMovies)
 	}
 }
+
+func BuildMockedGetMovieDetailHandler() func(c echo.Context) error {
+	return func(c echo.Context) error {
+		logger := logger.GetLogger()
+		logger.Info().Msg("[Mock] accessed GET movie detail")
+
+		var req GetMovieDetailRequest
+		if err := c.Bind(&req); err != nil {
+			return c.String(http.StatusBadRequest, "Bad Request")
+		}
+		logger.Info().Msg("successfully validated")
+
+		result := mock.MockedMovieDetailMapper[req.ID]
+
+		return c.JSON(http.StatusOK, result)
+	}
+}
