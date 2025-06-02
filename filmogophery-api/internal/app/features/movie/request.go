6f9e1e299f5f08ec6f122997233afa851072acc5
takeYY <t0k6yist@gmail.com>
2025-06-02 22:34:37 +0900

    add: schema to create a movie impression

--- filmogophery-api/internal/app/features/movie/request.go
+++ filmogophery-api/internal/app/features/movie/request.go
@@ -4,4 +4,12 @@ type (
 	GetMovieDetailRequest struct {
 		ID int32 `param:"id"`
 	}
+
+	PostMovieImpression struct {
+		ID         int32   `param:"id"`
+		WatchDate  string  `json:"watchDate"`
+		WatchMedia string  `json:"watchMedia"`
+		Rating     float32 `json:"rating"`
+		Note       string  `json:"note"`
+	}
 )
