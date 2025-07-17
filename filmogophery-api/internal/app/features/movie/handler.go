32852039443dc50ea2deec74b78113d68ef68499
takeYY <t0k6yist@gmail.com>
2025-07-17 22:28:40 +0900

    add: router and handler

--- filmogophery-api/internal/app/features/movie/handler.go
+++ filmogophery-api/internal/app/features/movie/handler.go
@@ -5,10 +5,40 @@ import (
 
 	"github.com/labstack/echo/v4"
 
+	"filmogophery/internal/app/routers"
 	"filmogophery/internal/app/services"
 	"filmogophery/internal/pkg/logger"
 )
 
+type (
+	getMoviesHandler struct {
+		interactor GetMoviesUseCase
+	}
+)
+
+func NewGetMoviesHandler(svc services.IServiceContainer) routers.IRoute {
+	return &getMoviesHandler{
+		interactor: NewGetMoviesInteractor(svc.MovieService()),
+	}
+}
+
+func (h *getMoviesHandler) Register(g *echo.Group) {
+	g.GET("/movies", h.handle)
+}
+
+func (h *getMoviesHandler) handle(c echo.Context) error {
+	logger := logger.GetLogger()
+	logger.Info().Msg("accessed GET movies")
+
+	result, err := h.interactor.Run(c.Request().Context())
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, result)
+}
+
+// NOTE: test でしか使っていないから移行したら消すこと
 func BuildGetMoviesHandler(
 	movieService services.IMovieService,
 ) func(c echo.Context) error {
