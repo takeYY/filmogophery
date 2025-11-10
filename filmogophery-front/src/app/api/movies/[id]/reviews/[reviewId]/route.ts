c2726d88c8e07607a95e999385276d77899cb0bf
takeYY <t0k6yist@gmail.com>
2025-11-10 21:28:31 +0900

    add: api router to create review history

--- filmogophery-front/src/app/api/movies/[id]/reviews/[reviewId]/route.ts
+++ filmogophery-front/src/app/api/movies/[id]/reviews/[reviewId]/route.ts
@@ -1,6 +1,40 @@
 import { APIBaseURL } from "@/constants/api";
 import { NextResponse } from "next/server";
 
+export async function POST(
+  req: Request,
+  { params }: { params: { _: string; reviewId: string } }
+) {
+  const url = `${APIBaseURL}/reviews/${params.reviewId}/history`;
+
+  try {
+    const requestData = await req.json();
+    const processedData = {
+      ...requestData,
+      platformId: parseInt(requestData.platformId), // int にする
+    };
+    console.log("リクエストデータ:", processedData);
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(processedData),
+    });
+    console.log(`APIレスポンスステータス: ${res.status}`);
+
+    return res;
+  } catch (error) {
+    console.error("Error in API route:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
 export async function PUT(
   req: Request,
   { params }: { params: { id: string; reviewId: string } }
