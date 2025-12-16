374f057c682ab0c637cd51c2b81551dd8a21e085
takeYY <t0k6yist@gmail.com>
2025-12-16 22:02:23 +0900

    feat: infinite scroll on home page

--- web-next-js/src/app/api/movies/route.ts
+++ web-next-js/src/app/api/movies/route.ts
@@ -1,7 +1,10 @@
 import { APIBaseURL } from "@/constants/api";
 
-export async function GET() {
-  const url = `${APIBaseURL}/movies`;
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const offset = searchParams.get("offset") || "0";
+
+  const url = `${APIBaseURL}/movies?offset=${offset}`;
   console.log("app apiから情報を取得中...");
 
   const res = await fetch(url);
