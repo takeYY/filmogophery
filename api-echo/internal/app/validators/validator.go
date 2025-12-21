1f4157cf2c0ab3c2b747dc07b456bc804a519308
takeYY <t0k6yist@gmail.com>
2025-12-21 20:23:58 +0900

    add: validation

---
+++ api-echo/internal/app/validators/validator.go
@@ -0,0 +1,30 @@
+package validators
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type Validator interface {
+	Validate() map[string][]string
+}
+
+func ValidateRequest(req Validator) map[string][]string {
+	return req.Validate()
+}
+
+func StructToErrors(err error) map[string][]string {
+	errors := make(map[string][]string)
+	if err == nil {
+		return errors
+	}
+
+	for _, err := range err.(validator.ValidationErrors) {
+		field := err.Field()
+		tag := err.Tag()
+		message := fmt.Sprintf("%s validation failed on %s", field, tag)
+		errors[field] = append(errors[field], message)
+	}
+	return errors
+}
