9b512422bdd1153feb7ed04cd954914336356d53
takeYY <t0k6yist@gmail.com>
2026-01-01 15:16:57 +0900

    add: scanner and valuer for DAO

--- api-echo/internal/pkg/constant/password.go
+++ api-echo/internal/pkg/constant/password.go
@@ -1,5 +1,32 @@
 package constant
 
+import (
+	"database/sql/driver"
+	"fmt"
+)
+
 type (
 	PasswordHasher string
 )
+
+// --- Scanner --- //
+// db の値を models の型に合わせて変換する
+
+func (s *PasswordHasher) Scan(value interface{}) error {
+	switch val := value.(type) {
+	case []uint8:
+		*s = PasswordHasher(val)
+	case string:
+		*s = PasswordHasher(val)
+	default:
+		return fmt.Errorf("unsupported type for PasswordHasher: %T", value)
+	}
+	return nil
+}
+
+// --- Valuer --- //
+// models の型を db の型に合わせて変換する
+
+func (s PasswordHasher) Value() (driver.Value, error) {
+	return string(s), nil
+}
