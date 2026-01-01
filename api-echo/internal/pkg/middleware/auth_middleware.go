23381a3e29256651534a8ea91990a8c78fb6d6d6
takeYY <t0k6yist@gmail.com>
2026-01-01 22:10:05 +0900

    add: auth middleware

---
+++ api-echo/internal/pkg/middleware/auth_middleware.go
@@ -0,0 +1,56 @@
+package middleware
+
+import (
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/labstack/echo/v4"
+
+	"filmogophery/internal/app/repositories"
+	"filmogophery/internal/pkg/config"
+	myJWT "filmogophery/internal/pkg/jwt"
+	"filmogophery/internal/pkg/logger"
+)
+
+func RequireAuthMiddleware(conf *config.Config, userRepo repositories.IUserRepository) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			logger := logger.GetLogger()
+
+			// Authorizationヘッダーからトークンを取得
+			authHeader := c.Request().Header.Get("Authorization")
+			if authHeader == "" {
+				return echo.ErrUnauthorized
+			}
+
+			// Bearer プレフィックスを削除
+			tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+			if tokenString == authHeader {
+				return echo.ErrUnauthorized
+			}
+
+			// JWT トークンを検証
+			token, err := jwt.ParseWithClaims(tokenString, &myJWT.JWTClaims{}, func(token *jwt.Token) (interface{}, error) {
+				return []byte(conf.Token.JWT_SECRET), nil
+			})
+			if err != nil || !token.Valid {
+				logger.Error().Msgf("invalid token: %s", err.Error())
+				return echo.ErrUnauthorized
+			}
+
+			claims, ok := token.Claims.(*myJWT.JWTClaims)
+			if !ok {
+				return echo.ErrUnauthorized
+			}
+
+			user, err := userRepo.FindByID(c.Request().Context(), int32(claims.UserID))
+			if err != nil {
+				logger.Error().Msgf("failed to fetch user: %s", err.Error())
+				return echo.ErrInternalServerError
+			}
+
+			c.Set("operator", user)
+			return next(c)
+		}
+	}
+}
