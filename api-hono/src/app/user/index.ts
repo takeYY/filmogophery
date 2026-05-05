4f53994fc2a403a73b85dd6e54210f698876d74d
takeYY <44844461+takeYY@users.noreply.github.com>
2026-05-05 14:07:59 +0900

    feat: endpoint to get current user

--- api-hono/src/app/user/index.ts
+++ api-hono/src/app/user/index.ts
@@ -1,6 +1,9 @@
 import { app } from "@/core/app";
 
 (await import("./handlers/create-user/create-user.handler")).default(app);
+(await import("./handlers/get-current-user/get-current-user.handler")).default(
+  app,
+);
 (await import("./handlers/get-user-points/get-user-points.handler")).default(
   app,
 );
